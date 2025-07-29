924c46e0e1b4a1f2283a1b145f97ae53b1ae15d1
nateweis <naftaliweiss21@gmail.com>
2025-07-28 20:54:07 -0400

    added back button to all pages

--- db/db_connection.js
+++ db/db_connection.js
@@ -17,7 +17,7 @@ module.exports = db
 
 /* 
     Next tasks: 
-    . add back button to all pages and refamilierize myself with whats going on with this app
+            O add back button to all pages and refamilierize myself with whats going on with this app
     . catch up on all the current data devorah has
     . have images store in db and display on screen
     . create a way to save images in db (includes paths and drag and drop elements)
